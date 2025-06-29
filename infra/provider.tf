1c00551416fbe295e033455e2e0b9256a015575c
Bakri-10 <>
2025-06-29 21:30:05 +0100

    Adding s3 backend

--- infra/provider.tf
+++ infra/provider.tf
@@ -8,6 +8,6 @@ terraform {
     key    = "terraform.tfstate"           # Path to store the state file within the bucket
     region = "us-east-1"                   # Your AWS region (change as needed)
     encrypt = true                         # Enable encryption of state file
-    # dynamodb_table = "terraform-locks"     # DynamoDB table name for state locking (create this table if not already)
+    dynamodb_table = "terraform-locks"     # DynamoDB table name for state locking (create this table if not already)
   }
 }
